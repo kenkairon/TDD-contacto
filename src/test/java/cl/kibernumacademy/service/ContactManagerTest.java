ecfc6c115068e0e1c34a55bc9431c6865ee315f0
kenkairon <ingeinfo2006@gmail.com>
2025-07-07 19:58:09 -0400

    Agregar modelo

--- src/test/java/cl/kibernumacademy/service/ContactManagerTest.java
+++ src/test/java/cl/kibernumacademy/service/ContactManagerTest.java
@@ -1,5 +1,26 @@
 package cl.kibernumacademy.service;
 
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class ContactManagerTest {
 
+    @Test
+    void agregarContacto_debeAgregarUnContactoAlaLista() {
+        // Se espera que exista una clase ContactManager con un método agregarContacto
+        ContactManager contactManager = new ContactManager();
+
+        // Se espera que exista una clase Contact con un constructor que reciba nombre,
+        // numero telefono y correo telefonico
+        Contact contacto = new Contact("Sofia", "005-9491091940", "sofia@correo.cl");
+
+        // Se espera que la instancia puedo acceder al metodo agregarContacto y que aña
+        // el contacto en una lista interna
+        contactManager.agregarContacto(contacto);
+
+        // Aqui validamos que la lista tenga solo un elemento
+        assertThat(contactManager.obtenerContactos(), hasSize(1));
+        assertThat(contactManger.obtenerContactos().get(0).getNombre(), is("Sofia"));
+
+    }
 }
